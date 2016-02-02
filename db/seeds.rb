8295c1adc8fa71866b967160e7ba26cb84665647
Lena Demski <Lenaski@web.de>
2016-02-02 13:42:25 +0100

    styled questionaries

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,23 +18,30 @@ Case.find_or_create_by(name: "Mega Menu")
 Case.find_or_create_by(name: "Dropdown mit Mega Menu")
 
 q1 = Questionary.find_or_create_by(name: "nasatlx")
-q2 = Questionary.find_or_create_by(name: "attrakdiff", description: 'Nachfolgend finden Sie Wortpaare, mit deren Hilfe Sie die Beurteilung vornehmen können. Sie stellen jeweils extreme Gegensätze dar, zwischen denen eine Abstufung möglich ist.Diese Bewertung bedeutet, dass das Produkt eher sympathisch, aber noch verbesserungsbedürftig ist.Denken Sie nicht lange über die Wortpaare nach, sondern geben Sie bitte die Einschätzung ab, die Ihnen spontan in den Sinn kommt. Vielleicht passen einige Wortpaare nicht so gut auf das Produkt, kreuzen Sie aber trotzdem bitte immer eine Antwort an. Denken Sie daran, dass es keine "richtigen" oder "falschen" Antworten gibt - nur Ihre persönliche Meinung zählt!')
+q2 = Questionary.find_or_create_by(name: "attrakdiff", description: 'Nachfolgend findest Du Wortpaare, mit deren Hilfe Du die Beurteilung vornehmen kannst. Sie stellen jeweils extreme Gegensätze dar, zwischen denen eine Abstufung möglich ist. Diese Bewertung bedeutet, dass das Produkt eher sympathisch, aber noch verbesserungsbedürftig ist.Denken Sie nicht lange über die Wortpaare nach, sondern geben Sie bitte die Einschätzung ab, die Ihnen spontan in den Sinn kommt. Vielleicht passen einige Wortpaare nicht so gut auf das Produkt, kreuzen Sie aber trotzdem bitte immer eine Antwort an. Denken Sie daran, dass es keine "richtigen" oder "falschen" Antworten gibt - nur Ihre persönliche Meinung zählt!')
 
-Question.find_or_create_by(questionary: q1, kind: "value", label: "Geistige Anforderung", description: "Wie viel geistige Anforderung war bei der Informationsaufnahme und bei der Informationsverarbeitung erforderlich (z.B. Denken, Entscheiden, Rechnen, Erinnern, Hinsehen, Suchen ...)? War die Aufgabe leicht oder anspruchsvoll, einfach oder komplex, erfordert sie hohe Genauigkeit oder ist sie fehlertolerant?")
-Question.find_or_create_by(questionary: q1, kind: "value", label: "Körperliche Anforderung", description: "Wie viel körperliche Aktivität war erforderlich (z.B. ziehen, drücken, drehen, steuern, aktivieren ...)? War die Aufgabe leicht oder schwer, einfach oder anstrengend, erholsam oder mühselig?")
-Question.find_or_create_by(questionary: q1, kind: "value", label: "Zeitliche Anforderung", description: "Wie viel Zeitdruck empfanden Sie hinsichtlich der Häufigkeit oder dem Takt mit dem die Aufgaben oder Aufgabenelemente auftraten? War die Aufgabe langsam und geruhsam oder schnell und hektisch?")
-Question.find_or_create_by(questionary: q1, kind: "value", label: "Leistung", description: "Wie erfolgreich haben Sie Ihrer Meinung nach die vom Versuchsleiter (oder Ihnen selbst) gesetzten Ziele erreicht? Wie zufrieden waren Sie mit Ihrer Leistung bei der Verfolgung dieser Ziele?")
-Question.find_or_create_by(questionary: q1, kind: "value", label: "Anstrengung", description: "Wie hart mussten Sie arbeiten, um Ihren Grad an Aufgabenerfüllung zu erreichen?")
-Question.find_or_create_by(questionary: q1, kind: "value", label: "Frustration", description: "Wie unsicher, entmutigt, irritiert, gestresst und verärgert (versus sicher, bestätigt, zufrieden, entspannt und zufrieden mit sich selbst) fühlten Sie sich während der Aufgabe?")
+Question.find_or_create_by(questionary: q1, kind: "value", label: "Geistige Anforderung", description: "Wie hoch war die geistige Anforderung der Aufgaben? (z.B. Denken, Entscheiden, Rechnen, Erinnern, Hinsehen, Suchen ...)?")
+Question.find_or_create_by(questionary: q1, kind: "value", label: "Körperliche Anforderung", description: "Wie viel körperliche Aktivität war erforderlich (z.B. ziehen, drücken, drehen, steuern, aktivieren ...)?")
+Question.find_or_create_by(questionary: q1, kind: "value", label: "Zeitliche Anforderung", description: "Wie viel Zeitdruck hast Du hinsichtlich der Häufigkeit oder des Takts mit dem die Aufgaben auftraten empfunden?")
+Question.find_or_create_by(questionary: q1, kind: "value", label: "Leistung", description: "Wie erfolgreich hast Du Deiner Meinung nach die gesetzten Ziele erreicht?")
+Question.find_or_create_by(questionary: q1, kind: "value", label: "Anstrengung", description: "Wie hart musstest Du arbeiten, um Deinen Grad an Aufgabenerfüllung zu erreichen?")
+Question.find_or_create_by(questionary: q1, kind: "value", label: "Frustration", description: "Wie unsicher, entmutigt, irritiert, gestresst hast Du Dich während der Aufgabe gefühlt?")
 
 Question.find_or_create_by(questionary: q2, kind: "comparison", label: "menschlich|technisch")
-Question.find_or_create_by(questionary: q2, kind: "comparison", label: "isolierend|verbindend")
 Question.find_or_create_by(questionary: q2, kind: "comparison", label: "angenehm|unangenehm")
-Question.find_or_create_by(questionary: q2, kind: "comparison", label: "originell|konventionell")
 Question.find_or_create_by(questionary: q2, kind: "comparison", label: "einfach|kompliziert")
-Question.find_or_create_by(questionary: q2, kind: "comparison", label: "fachmännisch|laienhaft")
 Question.find_or_create_by(questionary: q2, kind: "comparison", label: "hässlich|schön")
 Question.find_or_create_by(questionary: q2, kind: "comparison", label: "praktisch|unpraktisch")
-Question.find_or_create_by(questionary: q2, kind: "comparison", label: "sympathisch|unsympathisch")
 Question.find_or_create_by(questionary: q2, kind: "comparison", label: "umständlich|direkt")
+Question.find_or_create_by(questionary: q2, kind: "comparison", label: "voraussagbar|unberechenbar")
+Question.find_or_create_by(questionary: q2, kind: "comparison", label: "gut|schlecht")
+Question.find_or_create_by(questionary: q2, kind: "comparison", label: "verwirrend|übersichtlich")
+Question.find_or_create_by(questionary: q2, kind: "comparison", label: "motivierend|entmutigend")
+Question.find_or_create_by(questionary: q2, kind: "comparison", label: "herkömmlich|neuartig")
+Question.find_or_create_by(questionary: q2, kind: "comparison", label: "widerspenstig|handhabbar")
+
+
+
+
+
 
