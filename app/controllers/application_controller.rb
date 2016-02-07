26e340e65ec1b28d68766449aa09bb88272246ea
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2016-02-07 14:54:52 +0100

    prevent browser back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :force_user_to_journey
 
+  before_filter :set_cache_headers
+
+  private
+
+  def set_cache_headers
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
 
 
   def force_user_to_journey
