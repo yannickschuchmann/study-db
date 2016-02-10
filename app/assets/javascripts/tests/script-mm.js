9ddc78a25239e01330cd1ee3f7039180b8b1ef85
Lena Demski <Lenaski@web.de>
2016-02-10 12:44:21 +0100

    bug fixes & renaming

--- app/assets/javascripts/tests/script-mm.js
+++ app/assets/javascripts/tests/script-mm.js
@@ -15,42 +15,42 @@ $(function() {
      });
 
     /* Berechnung Index of Difficulty */
-    var $liStart = $('.layer-1 > li'); 
+    // var $liStart = $('.layer-1 > li'); 
 
-    $liStart.each(function(){
-      $currentElement = $(this);
-      var liStartHeight = $currentElement.outerHeight();
-      var liStartWidth = $currentElement.outerWidth();
-      var xStart = $currentElement.offset().left + liStartWidth/2;
-      var yStart = $currentElement.offset().top + liStartHeight;
+    // $liStart.each(function(){
+    //   $currentElement = $(this);
+    //   var liStartHeight = $currentElement.outerHeight();
+    //   var liStartWidth = $currentElement.outerWidth();
+    //   var xStart = $currentElement.offset().left + liStartWidth/2;
+    //   var yStart = $currentElement.offset().top + liStartHeight;
 
-      var layer2 = $currentElement.find('ul.layer-2');
-      $(layer2).each(function(){
-        var $childrenL2 = $(this).children();
-        $childrenL2.each(function(){
-          var $liEnd = $(this);
-          var liEndHeight = $liEnd.outerHeight();
-          var liEndWidth = $liEnd.outerWidth();
-          var xEnd = $liEnd.offset().left + liEndWidth/2;
-          var yEnd = $liEnd.offset().top + liEndHeight/2;
+    //   var layer2 = $currentElement.find('ul.layer-2');
+    //   $(layer2).each(function(){
+    //     var $childrenL2 = $(this).children();
+    //     $childrenL2.each(function(){
+    //       var $liEnd = $(this);
+    //       var liEndHeight = $liEnd.outerHeight();
+    //       var liEndWidth = $liEnd.outerWidth();
+    //       var xEnd = $liEnd.offset().left + liEndWidth/2;
+    //       var yEnd = $liEnd.offset().top + liEndHeight/2;
 
-          var alpha = getAngle(xStart, yStart, xEnd, yEnd);
-          var spanAngle = getSpanAngle();
-          if(alpha >= spanAngle){
-            var diagonal = getDiagonal(alpha);
-          }
-          else {
-            var diagonal = getLowDiagonale(alpha);
-          }
-          var dist = getHypothenuse(xStart, yStart, xEnd, yEnd);
-          var ID = getID(dist, diagonal);
+    //       var alpha = getAngle(xStart, yStart, xEnd, yEnd);
+    //       var spanAngle = getSpanAngle();
+    //       if(alpha >= spanAngle){
+    //         var diagonal = getDiagonal(alpha);
+    //       }
+    //       else {
+    //         var diagonal = getLowDiagonale(alpha);
+    //       }
+    //       var dist = getHypothenuse(xStart, yStart, xEnd, yEnd);
+    //       var ID = getID(dist, diagonal);
 
-          //console.debug($(this).children().first().text(), alpha * 180/Math.PI + "°", "Diagonale: " + diagonal, "Hypo: " + hypo, "Index: " + ID);
+    //       //console.debug($(this).children().first().text(), alpha * 180/Math.PI + "°", "Diagonale: " + diagonal, "Hypo: " + hypo, "Index: " + ID);
 
-          $(this).children().first().after(" <small>("+ID+")</small>");
-        });
-      });  
-    });
+    //       $(this).children().first().after(" <small>("+ID+")</small>");
+    //     });
+    //   });  
+    // });
   }
 });
     
--- app/assets/javascripts/tests/script.js
+++ app/assets/javascripts/tests/script.js
@@ -21,56 +21,56 @@ $(function() {
     });
 
     /* Berechnung Index of Difficulty */
-    var $liStart = $('.layer-1 > li');
-    $liStart.each(function(){
-      $currentElement = $(this);
-      var liStartHeight = $currentElement.outerHeight();
-      var liStartWidth = $currentElement.outerWidth();
-      var xStart = $currentElement.offset().left + liStartWidth/2;
-      var yStart = $currentElement.offset().top + liStartHeight;
+    // var $liStart = $('.layer-1 > li');
+    // $liStart.each(function(){
+    //   $currentElement = $(this);
+    //   var liStartHeight = $currentElement.outerHeight();
+    //   var liStartWidth = $currentElement.outerWidth();
+    //   var xStart = $currentElement.offset().left + liStartWidth/2;
+    //   var yStart = $currentElement.offset().top + liStartHeight;
 
-      var liLayer2 = $currentElement.find('ul.layer-2 >li');
-      $(liLayer2).each(function(){
-        var $currentLi = $(this);
-        var liEndHeight = $currentLi.outerHeight();
-        var liEndWidth = $currentLi.outerWidth();
-        var xEnd = $currentLi.offset().left + liEndWidth/2;
-        var yEnd = $currentLi.offset().top + liEndHeight/2;
+    //   var liLayer2 = $currentElement.find('ul.layer-2 >li');
+    //   $(liLayer2).each(function(){
+    //     var $currentLi = $(this);
+    //     var liEndHeight = $currentLi.outerHeight();
+    //     var liEndWidth = $currentLi.outerWidth();
+    //     var xEnd = $currentLi.offset().left + liEndWidth/2;
+    //     var yEnd = $currentLi.offset().top + liEndHeight/2;
 
-        var dist = getHypothenuse(xStart, yStart, xEnd, yEnd);
-        var width = $(this).find('span').outerHeight();
-        var ID1 = getID(dist, width);
+    //     var dist = getHypothenuse(xStart, yStart, xEnd, yEnd);
+    //     var width = $(this).find('span').outerHeight();
+    //     var ID1 = getID(dist, width);
 
-        width = ($(this).outerWidth())/2;
-        height = $(this).outerHeight();
-        var steering = getSteering(width, height)
-        $(this).children().first().after(" <small>("+ ID1 +")</small>");
-        var xStartLi2 = $currentLi.offset().left + liEndWidth;
-        var yStartLi2 = $currentLi.offset().top + liEndHeight/2;
+    //     width = ($(this).outerWidth())/2;
+    //     height = $(this).outerHeight();
+    //     var steering = getSteering(width, height)
+    //     $(this).children().first().after(" <small>("+ ID1 +")</small>");
+    //     var xStartLi2 = $currentLi.offset().left + liEndWidth;
+    //     var yStartLi2 = $currentLi.offset().top + liEndHeight/2;
 
-        var liLayer3 = $(this).find('.layer-3').children();
-        liLayer3.each(function(){
-          var $liEnd = $(this);
-          var liEndHeight = $liEnd.outerHeight();
-          var liEndWidth = $liEnd.outerWidth();
-          var xEnd = $liEnd.offset().left + liEndWidth/2;
-          var yEnd = $liEnd.offset().top + liEndHeight/2;
+    //     var liLayer3 = $(this).find('.layer-3').children();
+    //     liLayer3.each(function(){
+    //       var $liEnd = $(this);
+    //       var liEndHeight = $liEnd.outerHeight();
+    //       var liEndWidth = $liEnd.outerWidth();
+    //       var xEnd = $liEnd.offset().left + liEndWidth/2;
+    //       var yEnd = $liEnd.offset().top + liEndHeight/2;
 
-          var alpha = getAngle(xStartLi2, yStartLi2, xEnd, yEnd);
-          var spanAngle = getSpanAngle();
-          if(alpha >= spanAngle){
-            var diagonal = getDiagonal(alpha);
-          }
-          else {
-            var diagonal = getLowDiagonale(alpha);
-          }
-          var dist = getHypothenuse(xStartLi2, yStartLi2, xEnd, yEnd);
-          var ID2 = getID(dist, diagonal);
-          var IDSum = Math.round((ID1 + steering + ID2)*1000)/1000;
+    //       var alpha = getAngle(xStartLi2, yStartLi2, xEnd, yEnd);
+    //       var spanAngle = getSpanAngle();
+    //       if(alpha >= spanAngle){
+    //         var diagonal = getDiagonal(alpha);
+    //       }
+    //       else {
+    //         var diagonal = getLowDiagonale(alpha);
+    //       }
+    //       var dist = getHypothenuse(xStartLi2, yStartLi2, xEnd, yEnd);
+    //       var ID2 = getID(dist, diagonal);
+    //       var IDSum = Math.round((ID1 + steering + ID2)*1000)/1000;
 
-          $(this).children().first().after(" <small>("+ IDSum +")</small>");
-        });
-      });
-    });
+    //       $(this).children().first().after(" <small>("+ IDSum +")</small>");
+    //     });
+    //   });
+    // });
   }
 });
