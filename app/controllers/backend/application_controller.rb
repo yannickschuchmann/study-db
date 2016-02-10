35618b022ec9a2e5db0389469e088e3865ccfaaf
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2016-02-10 17:07:45 +0100

    added basic auth for backend

--- app/controllers/backend/application_controller.rb
+++ app/controllers/backend/application_controller.rb
@@ -1,3 +1,5 @@
 class Backend::ApplicationController < ApplicationController
+  http_basic_authenticate_with name: "studydb", password: "neverpushpasswordstogit"
+
   layout "backend/layouts/application"
 end
